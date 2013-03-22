90348555d2e886a90d0047dd8735c2f53f5f4c56
dustin <wdtseng@gmail.com>
2013-03-21 20:35:56 -0700

    test script that loads app engine libraries

---
+++ PurpleNumber/test/core_test/enums_test.py
@@ -0,0 +1,24 @@
+# coding=utf-8
+"""Unit test for enums.py.
+
+@author: Dustin Tseng
+"""
+import sys
+sys.path.insert(0, "../src")
+
+import unittest
+from core.enums import TianGanUtil
+from core.enums import TianGan
+
+class TestTianGanUtil(unittest.TestCase):
+    def test_chinese(self):
+        self.assertEqual("甲", TianGanUtil.chinese(0))
+        self.assertEqual("甲", TianGanUtil.chinese("JIA"))
+        self.assertEqual("甲", TianGanUtil.chinese(TianGan.JIA))
+        self.assertEqual("丁", TianGanUtil.chinese(3))
+        self.assertEqual("丁", TianGanUtil.chinese("DING"))
+        self.assertEqual("丁", TianGanUtil.chinese(TianGan.DING))
+
+if __name__ == '__main__':
+    unittest.main()
+
